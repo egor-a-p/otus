680eb562f96433405fb4764f1e6e4f9141c92345
egor-a-p <egor.a.petrov@gmail.com>
2017-08-07 09:13:13 +0300

    [HW11] : initial commit

---
+++ HW11/src/main/java/ru/otus/cache/CahceRegionFactory.java
@@ -0,0 +1,69 @@
+package ru.otus.cache;
+
+import java.util.Properties;
+
+import org.hibernate.boot.spi.SessionFactoryOptions;
+import org.hibernate.cache.CacheException;
+import org.hibernate.cache.spi.CacheDataDescription;
+import org.hibernate.cache.spi.CollectionRegion;
+import org.hibernate.cache.spi.EntityRegion;
+import org.hibernate.cache.spi.NaturalIdRegion;
+import org.hibernate.cache.spi.QueryResultsRegion;
+import org.hibernate.cache.spi.RegionFactory;
+import org.hibernate.cache.spi.TimestampsRegion;
+import org.hibernate.cache.spi.access.AccessType;
+
+/**
+ * @author e.petrov. Created 08 - 2017.
+ */
+public class CahceRegionFactory implements RegionFactory {
+	@Override
+	public void start(SessionFactoryOptions settings, Properties properties) throws CacheException {
+
+	}
+
+	@Override
+	public void stop() {
+
+	}
+
+	@Override
+	public boolean isMinimalPutsEnabledByDefault() {
+		return false;
+	}
+
+	@Override
+	public AccessType getDefaultAccessType() {
+		return null;
+	}
+
+	@Override
+	public long nextTimestamp() {
+		return 0;
+	}
+
+	@Override
+	public EntityRegion buildEntityRegion(String regionName, Properties properties, CacheDataDescription metadata) throws CacheException {
+		return null;
+	}
+
+	@Override
+	public NaturalIdRegion buildNaturalIdRegion(String regionName, Properties properties, CacheDataDescription metadata) throws CacheException {
+		return null;
+	}
+
+	@Override
+	public CollectionRegion buildCollectionRegion(String regionName, Properties properties, CacheDataDescription metadata) throws CacheException {
+		return null;
+	}
+
+	@Override
+	public QueryResultsRegion buildQueryResultsRegion(String regionName, Properties properties) throws CacheException {
+		return null;
+	}
+
+	@Override
+	public TimestampsRegion buildTimestampsRegion(String regionName, Properties properties) throws CacheException {
+		return null;
+	}
+}
---
+++ HW11/src/test/java/ru/otus/cache/CacheTest.java
@@ -0,0 +1,7 @@
+package ru.otus.cache;
+
+/**
+ * @author e.petrov. Created 08 - 2017.
+ */
+public class CacheTest {
+}
